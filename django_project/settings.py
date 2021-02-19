f4a2db5bcc169931fe580e29e38b0ba40501fce6
Andrew Merkur <amerkur@gmail.com>
2021-02-18 19:43:58 -0800

    Updated Static_Root 6

--- django_project/settings.py
+++ django_project/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
     }
 }
 
@@ -130,6 +130,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+django_heroku.settings(locals())
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
