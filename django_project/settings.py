4677bc3c8e88719a4afacb52e804e4fd9586d662
Andrew Merkur <amerkur@gmail.com>
2021-02-18 19:19:01 -0800

    Updated Static_Root 2

--- django_project/settings.py
+++ django_project/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = ''
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
