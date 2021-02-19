5c515047f5d7f2230abfd547cf072a362350723e
Andrew Merkur <amerkur@gmail.com>
2021-02-18 20:49:16 -0800

    Updated Static_Root 9

--- django_project/settings.py
+++ django_project/settings.py
@@ -157,3 +157,7 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
